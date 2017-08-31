40d2f286313da6d79319445e99b922a083afa27c
Tyler Spivey <tspivey@pcdesk.net>
2017-08-30 20:44:35 -0700

    Show revealed cards to watchers

--- server.py
+++ server.py
@@ -1331,11 +1331,13 @@ class MyDuel(dm.Duel):
 	def confirm_cards(self, player, cards):
 		pl = self.players[player]
 		op = self.players[1 - player]
-		pl.notify(pl._("{player} shows you {count} cards.")
-			.format(player=op.nickname, count=len(cards)))
-		for i, c in enumerate(cards):
-			pl.notify("%s: %s" % (i + 1, c.get_name(pl)))
-			self.revealed[(c.controller, c.location, c.sequence)] = True
+		players = [pl] + self.watchers
+		for pl in players:
+			pl.notify(pl._("{player} shows you {count} cards.")
+				.format(player=op.nickname, count=len(cards)))
+			for i, c in enumerate(cards):
+				pl.notify("%s: %s" % (i + 1, c.get_name(pl)))
+				self.revealed[(c.controller, c.location, c.sequence)] = True
 
 	def chain_solved(self, count):
 		self.revealed = {}
