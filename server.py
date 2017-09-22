7654d1247ee0e0d532888e918f12d57d1bcea16a
Tyler Spivey <tspivey@pcdesk.net>
2017-09-22 04:53:56 -0700

    Improve message

--- server.py
+++ server.py
@@ -340,7 +340,7 @@ class MyDuel(dm.Duel):
 			if pl is player:
 				pl.notify(pl._("you reveal your top deck card to be %s")%(card.get_name(pl)))
 			else:
-				pl.notify(pl._("%s reveals his top deck card to be %s")%(player.nickname, card.get_name(pl)))
+				pl.notify(pl._("%s reveals their top deck card to be %s")%(player.nickname, card.get_name(pl)))
 
 	def draw(self, player, cards):
 		pl = self.players[player]
