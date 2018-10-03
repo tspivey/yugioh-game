25891a38f43d8f160ad0e4a6795125cf4baf3dc1
Tyler Spivey <tspivey@pcdesk.net>
2018-10-03 06:53:01 -0700

    Update debug_duel.py

--- debug_duel.py
+++ debug_duel.py
@@ -1,22 +1,57 @@
 import sys
 import json
-import duel as dm
+import sqlite3
+from ygo import duel as dm
+from ygo import globals as glb
+from ygo import server
+from ygo.language_handler import LanguageHandler
+
+class FakePlayer:
+	def __init__(self, i, deck):
+		self.deck = {'cards': deck}
+		self.duel_player = i
+		self.cdb = glb.server.db
+		self.language = 'english'
+		self.watching = False
+		self.seen_waiting = False
+		self.soundpack = False
+		
+
+	def notify(self, text, *args, **kwargs):
+		print(self.duel_player, text)
+
+	_ = lambda self, t: t
+
+	@property
+	def strings(self):
+		return glb.language_handler.get_strings(self.language)
 
 def main():
 	global lr
 	lr=0
+	glb.language_handler = LanguageHandler()
+	glb.language_handler.add('english', 'en')
+	glb.language_handler.set_primary_language('english')
 	fp = open(sys.argv[1])
+	glb.server = server.Server()
+	glb.server.db = sqlite3.connect('locale/en/cards.cdb')
+	glb.server.db.row_factory = sqlite3.Row
 	for i, line in enumerate(fp):
 		print("line %d" % (i+1))
+		if i + 1 == 227:
+			import code;code.interact(local=dict(globals(), **locals()))
 		j = json.loads(line)
 		if 'data' in j:
 			j['data'] = j['data'].encode('latin1')
 		if j['event_type'] == 'start':
 			duel = dm.Duel(j.get('seed', 0))
-			duel.load_deck(0, j['deck0'], shuffle=False)
-			duel.load_deck(1, j['deck1'], shuffle=False)
+			players = [FakePlayer(i, deck) for i, deck in enumerate(j['decks'])]
+			for i, name in enumerate(j['players']):
+				players[i].nickname = name
+				duel.load_deck(players[i], shuffle=False)
+			duel.players = players
 			duel.cm.register_callback('*', debug)
-			duel.start()
+			duel.start(0x40000)
 		elif j['event_type'] == 'set_responsei':
 			duel.set_responsei(j['response'])
 			print("Responded %d" % j['response'])
@@ -28,9 +63,6 @@ def main():
 			if np != j['data']:
 				print("np data mismatch.")
 				return
-	print("here")
-	c = duel.get_cards_in_location(0, 4)[0]
-	import code;code.interact(local=dict(globals(), **locals()))
 
 def get_next_packet(duel):
 	res = dm.lib.process(duel.duel)
