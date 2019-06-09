5e133a7a47333dedb14a3035aa45fb3ae926c8bc
Toni Barth <software@satoprogs.de>
2019-06-09 18:51:13 +0200

    removed unneeded banlist method

--- ygo/banlist.py
+++ ygo/banlist.py
@@ -101,14 +101,3 @@ class Banlist:
 	@property
 	def name(self):
 		return self.__name
-
-	def limit(self, card):
-		if card.code in self.__cards:
-			return self.__cards[card.code]
-		if card.alias == 0:
-			return None
-		while card.alias != 0:
-			if card.alias in self.__cards:
-				return self.__cards[card.alias]
-			card = Card(card.alias)
-		return None
