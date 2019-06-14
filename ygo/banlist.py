892f1334f5e713b6e21976d781928dcd75550672
Toni Barth <software@satoprogs.de>
2019-06-14 15:46:59 +0200

    added banlist command to either list all available banlists or all cards related to a specific banlist

--- ygo/banlist.py
+++ ygo/banlist.py
@@ -1,6 +1,8 @@
 from .card import Card
 from . import globals
 
+import natsort
+
 class Banlist:
 	def __init__(self, name):
 		self.__cards = {}
@@ -97,6 +99,48 @@ class Banlist:
 
 	def check_and_resolve(self, cards):
 		return [(Card(e[0]), e[1], e[2], ) for e in self.check(cards)]
+
+	def __list_cards(self, cards, pl):
+
+		# getting all data
+		data = pl.cdb.execute('SELECT id, name FROM texts WHERE id IN ({0})'.format(', '.join([str(c) for c in cards]))).fetchall()
+
+		if len(data) < len(cards):
+			# not all cards are available in the player's language
+			# fill up with english ones
+			already_present = set([d[0] for d in data])
+
+			remaining = cards - already_present
+
+			data_r = globals.language_handler.primary_database.execute('SELECT id, name FROM texts WHERE id IN ({0})'.format(', '.join([str(c) for c in remaining]))).fetchall()
+
+			data = data + data_r
+
+		for d in natsort.natsorted(data, key = lambda d: d[1]):
+
+			pl.notify("\t" + d[1])
+
+	def show(self, pl):
+
+		all = set(self.__cards.keys())
+		
+		forbidden = set([c for c  in self.__cards.keys() if self.__cards[c] == 0])
+
+		limited = set([c for c in self.__cards.keys() if self.__cards[c] == 1])
+
+		semi_limited = all - forbidden - limited
+
+		pl.notify(pl._("Forbidden cards (not allowed at all):"))
+		
+		self.__list_cards(forbidden, pl)
+		
+		pl.notify(pl._("Limited cards (only allowed once):"))
+		
+		self.__list_cards(limited, pl)
+		
+		pl.notify(pl._("Semi-limited cards (only allowed twice):"))
+		
+		self.__list_cards(semi_limited, pl)
 
 	@property
 	def name(self):
