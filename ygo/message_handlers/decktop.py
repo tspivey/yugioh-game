e5830b0f179c56e777601e970401696e54a8547f
Toni Barth <software@satoprogs.de>
2019-07-10 20:11:35 +0200

    new message handler for excavate effects like sylvan

--- ygo/message_handlers/decktop.py
+++ ygo/message_handlers/decktop.py
@@ -2,6 +2,24 @@ import io
 
 from ygo.card import Card
 
+def msg_confirm_decktop(self, data):
+	cards = []
+	data = io.BytesIO(data[1:])
+	player = self.read_u8(data)
+	count = self.read_u8(data)
+	for i in range(count):
+		code = self.read_u32(data)
+		if code & 0x80000000:
+			code = code ^ 0x80000000 # don't know what this actually does
+		card = Card(code)
+		card.controller = self.read_u8(data)
+		card.location = self.read_u8(data)
+		card.sequence = self.read_u8(data)
+		cards.append(card)
+		
+	self.cm.call_callbacks('confirm_decktop', player, cards)
+	return data.read()
+
 def msg_decktop(self, data):
 	data = io.BytesIO(data[1:])
 	player = self.read_u8(data)
@@ -20,6 +38,16 @@ def decktop(self, player, card):
 		else:
 			pl.notify(pl._("%s reveals their top deck card to be %s")%(player.nickname, card.get_name(pl)))
 
-MESSAGES = {38: msg_decktop}
+def confirm_decktop(self, player, cards):
+	player = self.players[player]
+	for pl in self.players+self.watchers:
+		if pl is player:
+			pl.notify(pl._("you reveal the following cards from your deck:"))
+		else:
+			pl.notify(pl._("%s reveals the following cards from their deck:")%(player.nickname))
+		for i, c in enumerate(cards):
+			pl.notify("%d: %s"%(i+1, c.get_name(pl)))
 
-CALLBACKS = {'decktop': decktop}
+MESSAGES = {38: msg_decktop, 30: msg_confirm_decktop}
+
+CALLBACKS = {'decktop': decktop, 'confirm_decktop': confirm_decktop}
