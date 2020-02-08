e541c2ad925326a62a3da3d4887f21301ea3c255
Tyler Spivey <tspivey@pcdesk.net>
2020-02-07 17:11:49 -0800

    Update announce_card

--- ygo/message_handlers/announce_card.py
+++ ygo/message_handlers/announce_card.py
@@ -5,15 +5,19 @@ from twisted.internet import reactor
 from ygo.parsers.duel_parser import DuelParser
 from ygo.utils import process_duel
 from ygo import globals
+from ygo import duel
 
 def msg_announce_card(self, data):
 	data = io.BytesIO(data[1:])
 	player = self.read_u8(data)
-	type = self.read_u32(data)
-	self.cm.call_callbacks('announce_card', player, type)
+	size = self.read_u8(data)
+	options = []
+	for i in range(size):
+		options.append(self.read_u32(data))
+	self.cm.call_callbacks('announce_card', player, options)
 	return data.read()
 
-def announce_card(self, player, type):
+def announce_card(self, player, options):
 	pl = self.players[player]
 	def prompt():
 		pl.notify(pl._("Enter the name of a card:"))
@@ -25,7 +29,9 @@ def announce_card(self, player, type):
 		card = globals.server.get_card_by_name(pl, caller.text)
 		if card is None:
 			return error(pl._("No results found."))
-		if not card.type & type:
+		cd = duel.ffi.new('struct card_data *')
+		duel.card_reader_callback(card.code, cd)
+		if not duel.lib.declarable(cd, len(options), options):
 			return error(pl._("Wrong type."))
 		self.set_responsei(card.code)
 		reactor.callLater(0, process_duel, self)
