bd0b5ca9b1b04e2424a726decb0fb43b0bf0ce0f
Tyler Spivey <tspivey@pcdesk.net>
2020-07-03 09:14:41 -0700

    Fix showing cards to the wrong player

--- ygo/message_handlers/confirm_cards.py
+++ ygo/message_handlers/confirm_cards.py
@@ -18,12 +18,12 @@ def msg_confirm_cards(self, data):
 	return data.read()
 
 def confirm_cards(self, player, cards):
-	pl = self.players[player]
-	op = self.players[1 - player]
-	players = [pl] + self.watchers
+	player = self.players[cards[0].controller]
+	op = self.players[1 - cards[0].controller]
+	players = [op] + self.watchers
 	for pl in players:
 		pl.notify(pl._("{player} shows you {count} cards.")
-			.format(player=op.nickname, count=len(cards)))
+			.format(player=player.nickname, count=len(cards)))
 		for i, c in enumerate(cards):
 			pl.notify("%s: %s" % (i + 1, c.get_name(pl)))
 			self.revealed[(c.controller, c.location, c.sequence)] = True
