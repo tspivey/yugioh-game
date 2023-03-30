f54d1eb6932b31a2fc80953381a81b2dd0060ba1
Tyler Spivey <tspivey@pcdesk.net>
2023-03-30 06:32:50 -0700

    Fix deck loading

--- ygo/parsers/room_parser.py
+++ ygo/parsers/room_parser.py
@@ -269,8 +269,7 @@ def deck(caller):
 
 		deck = models.Deck.find(session, account, deck_name)
 
-	# and if the deck is alphanumeric, it's a deck name 
-	if deck_name.isalnum() and not deck: # if deck is a name and still hasn't been found
+	if not deck: # if deck is a name and still hasn't been found
 
 		deck = models.Deck.find(session, account, deck_name)
 
