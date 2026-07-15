603dc9ed7b4fc64db394fb6ee623946cf56ffe9f
Tyler Spivey <tspivey@pcdesk.net>
2026-07-15 15:20:44 -0700

    Fix Chaos Magical Hats

--- ygo/message_handlers/summoning.py
+++ ygo/message_handlers/summoning.py
@@ -1,7 +1,7 @@
 import io
 
 from ygo.card import Card
-from ygo.constants import TYPE
+from ygo.constants import TYPE, POSITION
 
 def msg_summoned(self, data):
 	return data[1:]
@@ -9,6 +9,8 @@ def msg_summoned(self, data):
 def msg_summoning(self, data, special=False):
 	data = io.BytesIO(data[1:])
 	code = self.read_u32(data)
+	if code == 0:
+		code = 483
 	card = Card(code)
 	card.set_location(self.read_location(data))
 	self.cm.call_callbacks('summoning', card, special=special)
@@ -18,13 +20,17 @@ def summoning(self, card, special=False):
 	nick = self.players[card.controller].nickname
 	for pl in self.players + self.watchers:
 		pos = card.get_position(pl)
+		if card.position != POSITION.FACEDOWN_DEFENSE:
+			name = card.get_name(pl)
+		else:
+			name = "Face-down card"
 		if special:
 			if card.type & TYPE.LINK:
-				pl.notify(pl._("%s special summoning %s (%d) in %s position.") % (nick, card.get_name(pl), card.attack, pos))
+				pl.notify(pl._("%s special summoning %s (%d) in %s position.") % (nick, name, card.attack, pos))
 			else:
-				pl.notify(pl._("%s special summoning %s (%d/%d) in %s position.") % (nick, card.get_name(pl), card.attack, card.defense, pos))
+				pl.notify(pl._("%s special summoning %s (%d/%d) in %s position.") % (nick, name, card.attack, card.defense, pos))
 		else:
-			pl.notify(pl._("%s summoning %s (%d/%d) in %s position.") % (nick, card.get_name(pl), card.attack, card.defense, pos))
+			pl.notify(pl._("%s summoning %s (%d/%d) in %s position.") % (nick, card.name, card.attack, card.defense, pos))
 
 def msg_summoning_special(self, *args, **kwargs):
 	kwargs['special'] = True
